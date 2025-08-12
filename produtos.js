1261771320faafa81a98d2f35fbb3aabd697badf
Cesar Augusto <100009078+Cesarleitor@users.noreply.github.com>
2025-08-11 23:01:15 -0300

    docs: raiz pronta

---
+++ produtos.js
@@ -0,0 +1,38 @@
+const produtos = [
+    {
+        nome: "Touro Walls Stret G",
+        preco: "R$ 350,00",
+        imagem: "./img/touroG.jpeg",
+        categoria: "animais"
+    },
+    {
+        nome: "Vaso Bojudo",
+        preco: "R$ 190,00",
+        imagem: "./img/vasoBojudo.jpeg",
+        categoria: "vasos"
+    },
+    {
+        nome: "Escultura Gorila P",
+        preco: "R$ 120,00",
+        imagem: "./img/gorilaP.png",
+        categoria: "animais"
+    },
+    {
+        nome: "Bambu Mosso de até 220cm",
+        preco: "R$ 110,00",
+        imagem: "./img/bambu220.jpeg",
+        categoria: "flores"
+    },
+    {
+        nome: "Arranjo com orquideas em vaso com musgo artificial",
+        preco: "R$ 80,00",
+        imagem: "./img/orquidea01.jpeg",
+        categoria: "flores"
+    },
+    {
+        nome: "Escultura Cavalo P",
+        preco: "R$ 130,00",
+        imagem: "./img/cavaloP.jpeg",
+        categoria: "animais"
+    }
+];
