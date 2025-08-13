4678a225e8acca2ce23c616b2663436606eca945
Cesar Augusto <100009078+Cesarleitor@users.noreply.github.com>
2025-08-12 22:54:04 -0300

    test: add img

--- produtos.js
+++ produtos.js
@@ -53,21 +53,21 @@ const produtos = [
         nome: "Garrafa Caras G",
         preco: "R$ 130,00",
         imagem: "./img/garrafa-caras-G.jpeg",
-        categoria: "esculturas"
+        categoria: "garrafas"
     },
 
     {
         nome: "Garrafa Caras M",
         preco: "R$ 130,00",
         imagem: "./img/garrafa-caras-M.jpeg",
-        categoria: "esculturas"
+        categoria: "garrafas"
     },
 
     {
         nome: "Garrafa Caras P",
         preco: "R$ 130,00",
         imagem: "./img/garrafa-caras-P.jpeg",
-        categoria: "esculturas"
+        categoria: "garrafas"
     },
 
     {
@@ -89,6 +89,218 @@ const produtos = [
         imagem: "./img/Mascara Torre M.jpeg",
         categoria: "esculturas"
     },
+     {
+        nome: "Pantera Sentada G",
+        preco: "R$ 130,00",
+        imagem: "./img/pantera-sentadaG.jpeg",
+        categoria: "animais"
+    },
+     {
+        nome: "Touro Bravo",
+        preco: "R$ 130,00",
+        imagem: "./img/touro_bravoP.jpeg",
+        categoria: "animais"
+    },
+     {
+        nome: "Mascara Silêncio P",
+        preco: "R$ 130,00",
+        imagem: "./img/silencioP.jpeg",
+        categoria: "esculturas"
+    },
+     {
+        nome: "Leão Sentado P",
+        preco: "R$ 130,00",
+        imagem: "./img/leao_sentadoP.jpeg",
+        categoria: "animais"
+    },
+     {
+        nome: "Pantera Ataque G",
+        preco: "R$ 130,00",
+        imagem: "./img/ataqueG.jpeg",
+        categoria: "animais"
+    },
+     {
+        nome: "Pantera Ataque PP-P-M",
+        preco: "R$ 130,00",
+        imagem: "./img/ataque.jpeg",
+        categoria: "animais"
+    },
+     {
+        nome: "Pantera Ataque GG",
+        preco: "R$ 130,00",
+        imagem: "./img/ataqueGG.jpeg",
+        categoria: "animais"
+    },
+     {
+        nome: "Pantera Caminhando GG ",
+        preco: "R$ 130,00",
+        imagem: "./img/caminhandoGG.jpeg",
+        categoria: "animais"
+    },
+
+     {
+        nome: "Pantera Bebendo Água",
+        preco: "R$ 130,00",
+        imagem: "./img/bebendo-agua.jpeg",
+        categoria: "animais"
+    },
+     {
+        nome: "Pntera Sentada P",
+        preco: "R$ 130,00",
+        imagem: "./img/pantera-sentadaP.jpeg",
+        categoria: "animais"
+    },
+     {
+        nome: "Águia P - M",
+        preco: "R$ 130,00",
+        imagem: "./img/urso-sentado.jpeg",
+        categoria: "animais"
+    },
+     {
+        nome: "Urso Sentado P - M",
+        preco: "R$ 130,00",
+        imagem: "./img/cachorro-deitado.png",
+        categoria: "animais"
+    },
+     {
+        nome: "Urso Panda",
+        preco: "R$ 130,00",
+        imagem: "./img/urso-panda.jpeg",
+        categoria: "animais"
+    },
+     {
+        nome: "Buldogue PP - P",
+        preco: "R$ 130,00",
+        imagem: "./img/buldogue.jpeg",
+        categoria: "animais"
+    },
+     {
+        nome: "Gato PP",
+        preco: "R$ 130,00",
+        imagem: "./img/gatoPP.png",
+        categoria: "animais"
+    },
+     {
+        nome: "Leopardo PP",
+        preco: "R$ 130,00",
+        imagem: "./img/leopardo.jpeg",
+        categoria: "animais"
+    },
+     {
+        nome: "Pantera Deitada",
+        preco: "R$ 130,00",
+        imagem: "./img/pantera-deitada.jpeg",
+        categoria: "animais"
+    },
+     {
+        nome: "Buldogue PP - P",
+        preco: "R$ 130,00",
+        imagem: "./img/buldogue.jpeg",
+        categoria: "animais"
+    },
+     {
+        nome: "Buldogue PP - P",
+        preco: "R$ 130,00",
+        imagem: "./img/buldogue.jpeg",
+        categoria: "animais"
+    },
+     {
+        nome: "Escultura Lisa",
+        preco: "R$ 130,00",
+        imagem: "./img/escultura-lisa.jpeg",
+        categoria: "esculturas"
+    },
+     {
+        nome: "Escultura Mascara Teatro",
+        preco: "R$ 130,00",
+        imagem: "./img/escultura-teatro.jpeg",
+        categoria: "esculturas"
+    },
+     {
+        nome: "Escultura Dragão",
+        preco: "R$ 130,00",
+        imagem: "./img/escultura-dragao.jpeg",
+        categoria: "animais"
+    },
+     {
+        nome: "Tubarão Parede",
+        preco: "R$ 130,00",
+        imagem: "./img/tubarao.jpeg",
+        categoria: "animais"
+    },
+     {
+        nome: "Barco",
+        preco: "R$ 130,00",
+        imagem: "./img/escultura-barco.jpeg",
+        categoria: "esculturas"
+    },
+     {
+        nome: "Busto Pantera",
+        preco: "R$ 130,00",
+        imagem: "./img/busto-pantera.jpeg",
+        categoria: "animais"
+    },
+     {
+        nome: "Leão com Base PP",
+        preco: "R$ 130,00",
+        imagem: "./img/leao-com-base.jpeg",
+        categoria: "animais"
+    },
+       {
+        nome: "Garrafa Vintage",
+        preco: "R$ 130,00",
+        imagem: "./img/garrafa-flordeliz.jpeg",
+        categoria: "garrafas"
+    },
+
+       {
+        nome: "Garrafas Listrada",
+        preco: "R$ 130,00",
+        imagem: "./img/garrafas-listrada.jpeg",
+        categoria: "garrafas"
+    },
+
+       {
+        nome: "Garrafa Treliça",
+        preco: "R$ 130,00",
+        imagem: "./img/garrafa-treliça.jpeg",
+        categoria: "garrafas"
+    },
+
+       {
+        nome: "Garrafa Retangulo",
+        preco: "R$ 130,00",
+        imagem: "./img/garrafa-retangulo.jpeg",
+        categoria: "garrafas"
+    },
+ {
+        nome: "Garrafa Quadrada",
+        preco: "R$ 130,00",
+        imagem: "./img/garrafa-quadrada.jpeg",
+        categoria: "garrafas"
+    },
+     {
+        nome: "Vso Liso Aberto",
+        preco: "R$ 130,00",
+        imagem: "./img/vaso-flordeliz.jpeg",
+        categoria: "vasos"
+    },
+
+     {
+        nome: "Vaso Irregular P - M - G",
+        preco: "R$ 130,00",
+        imagem: "./img/vaso-irregular.jpeg",
+        categoria: "vasos"
+    },
+
+     {
+        nome: "Vaso Liso P - M - G",
+        preco: "R$ 130,00",
+        imagem: "./img/vaso-liso.jpeg",
+        categoria: "vasos"
+    },
+
+
 
 
 
